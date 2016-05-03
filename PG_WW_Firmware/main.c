c6dc3895c2a68206cf401cd245abffd994404f5f
Jens Drenhaus <jens.drenhaus@tu-dortmund.de>
2016-05-03 13:56:15 +0200

    fixed some merge fails

--- PG_WW_Firmware/main.c
+++ PG_WW_Firmware/main.c
@@ -28,7 +28,6 @@ void main(void) {
     TA1CCR0 = 62500;                 // SMCLK/8/62500 = 2Hz => 0,5s
     TA1CCTL0 |= CCIE;                // TACCR0 interrupt enabled
 
-<<<<<<< HEAD
     //#########################################################################
     // Configure UART A0 for debugging (backchannel uart)
     //#########################################################################
@@ -74,33 +73,25 @@ void main(void) {
 
     PM5CTL0	&=	~LOCKLPM5;			// Disable the GPIO power-on default
     								//               high-impedance mode
-=======
+
     // Configure UART A0
 
-     P2SEL1 |= BIT0 | BIT1;          // Set port function to UART
-     P2SEL0 &= ~(BIT0 | BIT1);       // Set port function to UART
+    P2SEL1 |= BIT0 | BIT1;          // Set port function to UART
+    P2SEL0 &= ~(BIT0 | BIT1);       // Set port function to UART
 
-     UCA0CTLW0 = UCSWRST;            // Put eUSCI in reset
-     UCA0CTLW0 |= UCSSEL__SMCLK;     // CLK = SMCLK
-     // Baud Rate calculation
-     // 1000000/(16*9600) = 6.510
-     // Fractional portion = 0.510
-     // User's Guide Table 21-4: UCBRSx = 0xAA
-     // UCBRFx = int ( (6.510-6)*16) = 8
-     UCA0BR0 = 6;                    // 8000000/16/9600
-     UCA0BR1 = 0x00;                 // UCA0BR is a word register, set high byte
-     UCA0MCTLW |= UCOS16 | UCBRF_8 | 0xAA00;
-     UCA0CTLW0 &= ~UCSWRST;          // Initialize eUSCI
-     UCA0IE |= UCRXIE;               // Enable RX Interrupt
-    // Configure GPIO
+    UCA0CTLW0 = UCSWRST;            // Put eUSCI in reset
+    UCA0CTLW0 |= UCSSEL__SMCLK;     // CLK = SMCLK
+    // Baud Rate calculation
+    // 1000000/(16*9600) = 6.510
+    // Fractional portion = 0.510
+    // User's Guide Table 21-4: UCBRSx = 0xAA
+    // UCBRFx = int ( (6.510-6)*16) = 8
+    UCA0BR0 = 6;                    // 8000000/16/9600
+    UCA0BR1 = 0x00;                 // UCA0BR is a word register, set high byte
+    UCA0MCTLW |= UCOS16 | UCBRF_8 | 0xAA00;
+    UCA0CTLW0 &= ~UCSWRST;          // Initialize eUSCI
+    UCA0IE |= UCRXIE;               // Enable RX Interrupt
 
-    PM5CTL0 &=  ~LOCKLPM5;          // Disable the GPIO power-on default high-impedance mode
->>>>>>> de85d3f631a374f41b578596c22a6413fff42b17
-
-    P1OUT &= 0x00;                  // Shut down everything
-    P1DIR &= 0x00;
-    P4OUT &= 0x00;                  // Shut down everything
-    P4DIR &= 0x00;
     // Configure GPIO LEDs
     P1DIR |= BIT0;                  // Set P1.0 to output direction
     P4DIR |= BIT6;                  // Set P4.6 to output direction
@@ -169,7 +160,6 @@ __interrupt void Timer0_A0(void)
 #pragma vector=TIMER1_A0_VECTOR
 __interrupt void Timer0_A1(void)
 {
-<<<<<<< HEAD
 	static int num = 0x30;
 	static int tick = 1;        // set state for full secound
 
@@ -212,27 +202,10 @@ __interrupt void USCI_A1_ISR(void)
     case USCI_UART_UCTXCPTIFG: break;
   }
 }
-=======
-    static int num = 0x30;
-    static int tick = 1;        // set state for full secound
 
-    if (tick) {
-        while(!(UCA0IFG&UCTXIFG));
-        UCA0TXBUF = num;
-        num++;
-        if (num > '9') {
-            num = 0x30;
-            while(!(UCA0IFG&UCTXIFG));
-            UCA0TXBUF = 0x0A;           // send NL
-            while(!(UCA0IFG&UCTXIFG));
-            UCA0TXBUF = 0x0D;           // send CR
-        }
-    }
 
-    tick    ^=  1;      // toggle tick state
-//  P4OUT   ^=  BIT6;   // toggle red LED
 
-}
+
 
 #pragma vector=USCI_A0_VECTOR
 __interrupt void USCIA0RX_ISR(void)
@@ -246,90 +219,12 @@ __interrupt void USCIA0RX_ISR(void)
 
     if (received=='g')
     {
-        P1OUT ^= BIT0;              // toggle green LED
+        P1OUT ^= BIT0;      // toggle green LED
     }
     else if (received == 'r')
     {
         P4OUT   ^=  BIT6;   // toggle red LED
->>>>>>> de85d3f631a374f41b578596c22a6413fff42b17
 
     }
 
 }
-
-
-
-
-<<<<<<< HEAD
-=======
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//#define ONE_SECOND 800000
-//
-//volatile unsigned int state = 0;
-//
-//
-//
-//int button_klicked();
-//
-//int main(void) {
-//    WDT_A_hold(WDT_A_BASE);                                   // Stop watchdog timer
-//
-//    GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN0);
-//    GPIO_setOutputLowOnPin( GPIO_PORT_P1, GPIO_PIN0 );
-//    GPIO_setAsInputPinWithPullUpResistor(GPIO_PORT_P1, GPIO_PIN1);
-//
-//    PMM_unlockLPM5();                                         // unlock pins (fr5969 spezific pin state after reset)
-//
-//
-//    while(1) {
-//
-//        if ( button_klicked()) {
-//
-//          if (state == 0) {
-//              // turn on LED
-//              GPIO_setOutputHighOnPin( GPIO_PORT_P1, GPIO_PIN0 );
-//              state = 1;
-//              continue;
-//          }
-//          if (state == 1) {
-//              // turn off LED
-//              GPIO_setOutputLowOnPin( GPIO_PORT_P1, GPIO_PIN0 );
-//              state = 0;
-//              continue;
-//          }
-//        }
-//    }
-//
-//  return 0;
-//}
-//
-//int button_klicked() {
-//  static unsigned int pinVal;
-//  pinVal = GPIO_getInputPinValue( GPIO_PORT_P1, GPIO_PIN1 );
-//
-//  if(pinVal == 0) {
-//      _delay_cycles(1600);                                                // ca 2ms debounce
-//      do {
-//          pinVal = GPIO_getInputPinValue( GPIO_PORT_P1, GPIO_PIN1 );      // wait for button to be released
-//      } while (pinVal == 0);
-//      _delay_cycles(1600);                                                // ca 2ms debounce
-//      return 1;
-//  }
-//
-//  return 0;
-//}
->>>>>>> de85d3f631a374f41b578596c22a6413fff42b17
