9ccd4e5d74a7c715a9e31b79830bf6b89cb12107
Jens <jens.drenhaus@tu-dortmund.de>
2016-05-30 18:35:22 +0200

    try to fix 2

--- PG_WW_Firmware/main.c
+++ PG_WW_Firmware/main.c
@@ -1,10 +1,13 @@
-#include "phy.h"
+#include <phy.h>
 #include "msp430fr5969.h"
 #include <string.h>
+<<<<<<< HEAD
 //#include "ui.h"
+=======
+>>>>>>> parent of 168c385... changes faild- serial does not work anymore
 
 // Globals
-
+int pressed = 0;
 const int MY_BOX_ID = 42;
 
 // Defines
@@ -21,7 +24,7 @@ Sensor_Data sensor_data = {0.0};
 
 // Prototypes
 void process(char *string);
-void enter();
+
 
 
 
@@ -30,7 +33,14 @@ void main(void) {
 
     WDTCTL = WDTPW | WDTHOLD;           // Stop watchdog timer
 
-    UI_init(enter);
+    //#########################################################################
+    // Configure Timer A0 used for debouncing
+    //#########################################################################
+
+    TA0CTL |= (TASSEL__SMCLK | MC__UP | ID__8);          // SMCLK/8 , UP mode
+    TA0CTL |= TACLR;                 // clear to acticate new clock settings
+    TA0CCR0 = 5000;                  // SMCLK/8/5000 = 25Hz => 40ms
+    TA0CCTL0 &= ~CCIE;               // TACCR0 interrupt disabled
 
     //#########################################################################
     // Configure Timer A1 used for periodic actions
@@ -41,10 +51,14 @@ void main(void) {
     TA1CCR0 = 62500;                 // SMCLK/8/62500 = 2Hz => 0,5s
     TA1CCTL0 |= CCIE;                // TACCR0 interrupt enabled
 
+    //#########################################################################
+    // Configure UART A0 for debugging (backchannel uart)
+    //#########################################################################
+
     PHY_init(process);
 
     //#########################################################################
-    // Configure UART A1 for measurement data input
+    // Configure UART A1 for measurement datat input
     //#########################################################################
 
     P2SEL1 |= BIT0 | BIT1;          // Set port function to UART
@@ -63,7 +77,40 @@ void main(void) {
     UCA1CTLW0 &= ~UCSWRST;          // Initialize eUSCI
     UCA1IE |= UCRXIE;               // Enable USCI_A1 RX interrupt
 
+    //#########################################################################
+    // Configure GPIO basic user interface
+    //#########################################################################
 
+    PM5CTL0	&=	~LOCKLPM5;			// Disable the GPIO power-on default
+    								//               high-impedance mode
+
+
+    P2SEL1 |= BIT0 | BIT1;          // Set port function to UART
+    P2SEL0 &= ~(BIT0 | BIT1);       // Set port function to UART
+
+    UCA0CTLW0 = UCSWRST;            // Put eUSCI in reset
+    UCA0CTLW0 |= UCSSEL__SMCLK;     // CLK = SMCLK
+    // Baud Rate calculation
+    // 1000000/(16*9600) = 6.510
+    // Fractional portion = 0.510
+    // User's Guide Table 21-4: UCBRSx = 0xAA
+    // UCBRFx = int ( (6.510-6)*16) = 8
+    UCA0BR0 = 6;                    // 8000000/16/9600
+    UCA0BR1 = 0x00;                 // UCA0BR is a word register, set high byte
+    UCA0MCTLW |= UCOS16 | UCBRF_8 | 0xAA00;
+    UCA0CTLW0 &= ~UCSWRST;          // Initialize eUSCI
+    UCA0IE |= UCRXIE;               // Enable RX Interrupt
+
+    // Configure GPIO LEDs
+    P1DIR |= BIT0;                  // Set P1.0 to output direction
+    P4DIR |= BIT6;                  // Set P4.6 to output direction
+    // Configure GPIO Button
+    P1DIR &= ~BIT1;                 // Set P1.1 to input direction
+    P1REN |= BIT1;                  // Set P1.1 pullup/down Resistor
+    P1OUT |= BIT1;                  // Select P1.1 pull-up
+    P1IE  |= BIT1;                  // Enable Interrupt on P1.1
+    P1IES |= BIT1;                  // High/Low Edge
+    P1IFG &= ~BIT1;                 // clear P1.1 interrupt flag
 
 
 
@@ -80,10 +127,46 @@ void process (char* string) {
 	PHY_send(string);
 }
 
-void enter(){
-	UI_status_toggle();
+//#############################################################################
+// Port 1.1 ISR for button debounce
+//#############################################################################
+#pragma vector=PORT1_VECTOR
+__interrupt void Port_1(void)
+{
+    P1IE &= ~BIT1;                  // Disable Interrupt on P1.1
+    P1IFG &= ~BIT1;                 // clear P1.1 interrupt flag
+    TA0CCTL0 |= CCIE;               // TACCR0 interrupt enabled
+    TA0CTL |= TACLR;                // start Timer TA0
+    pressed = 1;                    // save buttonstate
 }
 
+//#############################################################################
+// Timer A0 ISR for button debounce
+//#############################################################################
+#pragma vector=TIMER0_A0_VECTOR
+__interrupt void Timer0_A0(void)
+{
+    // check if button is still pressed
+    if (!(P1IN & BIT1) && pressed) {
+        TA0CTL |= TACLR;            // reset Timer TA0
+    }
+    // button released ?
+    else if ((P1IN & BIT1) && pressed) {
+
+        // ### button released action ###
+        while(!(UCA0IFG&UCTXIFG));
+        UCA0TXBUF = 0x0A;           // send NL
+        while(!(UCA0IFG&UCTXIFG));
+        UCA0TXBUF = 0x0D;           // send CR
+        P1OUT ^= BIT0;              // toggle green LED
+        // ###############################
+
+        pressed = 0;
+        TA0CCTL0 &= ~CCIE;          // TACCR0 interrupt disabled
+        P1IFG &= ~BIT1;             // clear P1.1 interrupt flag
+        P1IE |= BIT1;               // Enable Interrupt on P1.1
+    }
+}
 
 //#############################################################################
 // Timer A1 ISR for periodic events
@@ -99,7 +182,7 @@ __interrupt void Timer0_A1(void)
 	}
 
 	tick ^= 1;                   // toggel tick state
-	UI_tick();
+	P4OUT ^= BIT6;				 // toggel red LED
 
 }
 
---
+++ templ.h
@@ -0,0 +1,54 @@
+/*------------------------------------------------------------------------------
+| File: name.h 
+|   
+| Say something about WHAT the model does.
+|
+| Note: important things to know
+|-------------------------------------------------------------------------------
+| Datatypes:
+|     NAME_datatype    -- used to store data and informatin of something
+|     ...
+|-------------------------------------------------------------------------------
+| Functions:
+|     name_somefunction  -- short explaination
+|     ...
+ -----------------------------------------------------------------------------*/
+
+#ifndef _NAME_H_                                        
+#define _NAME_H_
+
+#define NAME_SOMEDEFS		42                    1
+
+
+//#############################################################################
+// datatypes
+
+/* structure to hold details of ... */ 
+typedef struct s_name {
+		int varxy
+}NAME_dinx_t;
+
+
+
+//#############################################################################
+// callback function
+typedef void (*Callback)(arg*);
+
+//#############################################################################
+// functions (prototypes)
+
+/*------------------------------------------------------------------------------
+| name_somefunction  -- short explaination  
+|   
+| Parameter:
+|     --- 
+|     
+| Return:
+|     ---
+| 
+ -----------------------------------------------------------------------------*/
+extern void name_somefunction();
+
+
+
+#endif // _NAME_H_ 
